51f96d2ca368674f1d984ec4d638ee9df2a4ae44
My Name Kotz <89704741+Kotzyy@users.noreply.github.com>
2021-11-28 19:56:48 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-let express = require('express'),
-cors = require('cors'),
+const express = require('express');
+const app = express();
+const cors = require('cors');
 secure = require('ssl-express-www');
 let PORT = process.env.PORT || 8080 || 5000 || 3000
-let { color } = require('./lib/color.js')
+let { color } = require('./lib/color.js');
 
-let home = require('./routes/main'),
+let home = require('./routes/main');
 
-let app = express()
 app.enable('trust proxy');
-app.set("json spaces",2)
-app.use(cors())
-app.use(secure)
+app.set("json spaces",2);
+app.use(cors());
+app.use(secure);
 
-app.use('/', home)
+app.use('/', home);
 
 app.listen(PORT, () => {
     console.log(color("Server running on port " + PORT,'green'))
-})
+});
 
 module.exports = app
